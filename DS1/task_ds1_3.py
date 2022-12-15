918d94388a3c6721ced1cd74f1ea916b7ea1e3bd
Iren <kovalenkovaiv@gmail.com>
2022-12-15 15:28:37 +0400

    task345

--- DS1/task_ds1_3.py
+++ DS1/task_ds1_3.py
@@ -6,3 +6,17 @@
 # - x=34; y=-30 -> 4
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
+
+print('Введите координаты:  ')
+x = int(input('x:'))
+y = int(input('y:'))
+if x > 0 and y > 0:
+    print('1 четверть')
+elif x > 0 and y < 0:
+    print('2 четверть')
+elif x < 0 and y < 0:
+    print('3 четверть')
+elif x > 0 and y < 0:
+    print('4 четверть')
+else:
+    print('Координаты x и y не должны быть равны 0')
--- DS1/task_ds1_5.py
+++ DS1/task_ds1_5.py
@@ -9,3 +9,11 @@
 
 # out
 # 5.099
+print('Введите координаты двух точек')
+x1 = int(input('x1: '))
+y1 = int(input('y1: '))
+x2 = int(input('x2: '))
+y2 = int(input('y2: '))
+
+ab = (((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5)
+print(f"{ab:.3f}")
