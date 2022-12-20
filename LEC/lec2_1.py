d62f5419efababf332a4f674a346cfd183af1aa4
Iren <kovalenkovaiv@gmail.com>
2022-12-20 16:03:37 +0400

    kmk

---
+++ LEC/lec2_1.py
@@ -0,0 +1,13 @@
+# colors = ['red', 'green','blue']
+# data = open('file.txt', 'a')
+# data.write('LINE 121\n')
+# data.write('LINE 122\n')
+# data.write('Hello\n')
+# data.close()
+
+# path = 'file.txt'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
+
