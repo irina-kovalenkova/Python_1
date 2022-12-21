55224453f01487d2596434319a1ce26512825dfb
Iren <kovalenkovaiv@gmail.com>
2022-12-21 22:15:44 +0400

    dfg

--- SEM/task1_1.py
+++ SEM/task1_1.py
@@ -1,8 +1,13 @@
-a = int(input())
-a1 = int(input())
-b = int(input())
-b1 = int(input())
-if (((a1 - a) <= 1) and ((a1 - a) >= -1)) and (((b1 - b) <= 1) and ((b1 - b) >= -1)):
-    print('YES')
-else:
-    print('NO')
+# a = int(input())
+# a1 = int(input())
+# b = int(input())
+# b1 = int(input())
+# if (((a1 - a) <= 1) and ((a1 - a) >= -1)) and (((b1 - b) <= 1) and ((b1 - b) >= -1)):
+#     print('YES')
+# else:
+#     print('NO')
+
+def Hello():
+    print('Hello word!')
+
+Hello()
