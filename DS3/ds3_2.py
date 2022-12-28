6f1fb52cf1e6e093c42e077487e656824268b8d0
Iren <kovalenkovaiv@gmail.com>
2022-12-28 23:10:38 +0400

    dfg

--- DS3/ds3_2.py
+++ DS3/ds3_2.py
@@ -16,13 +16,40 @@
 
 
 from random import sample
-len_list = (int(input('Введите длину списка: ')))
+# len_list = (int(input('Введите длину списка: ')))
 
-new_list = sample(range(1,len_list+1), k=len_list)
-print('Созданный массив: ', new_list)
+# new_list = sample(range(1,len_list+1), k=len_list)
+# print('Созданный массив: ', new_list)
 
-comp_list = []
-for i in range((len(new_list) // 2 + len(new_list) % 2)):
-    new_num = new_list[i] * new_list[-i-1]
-    comp_list.append(new_num)
-print('Произведения: ',comp_list)
+# comp_list = []
+# for i in range((len(new_list) // 2 + len(new_list) % 2)):
+#     new_num = new_list[i] * new_list[-i-1]
+#     comp_list.append(new_num)
+# print('Произведения: ',comp_list)
+
+#____________________________________________________________________________
+
+#решение учителя
+
+def list_rand_nums(count):
+    if count < 0:
+        print("Negative value of the number of numbers!")
+        return []
+    
+    list_nums = sample(range(1, count * 2), count)
+    return list_nums
+
+def prod_pairs(list_nums: list):
+    res_list = []
+    len_list = len(list_nums)
+    
+    for k in range(len_list//2):
+        res_list.append(list_nums[k] * list_nums[len_list - k -1])
+        
+    if len_list % 2:
+        res_list.append(list_nums[len_list // 2])
+    return res_list
+    
+all_list = list_rand_nums(int(input('Number of numbers: ')))
+print(all_list)
+print(prod_pairs(all_list))        
--- DS3/ds3_3.py
+++ DS3/ds3_3.py
@@ -10,13 +10,28 @@
 # out
 # 1011
 
-user_n = int(input('Введите число: '))
-n = user_n
-num_list = []
-while n > 0:
-    num = n % 2
-    num_list.append(str(num))
-    n = n // 2
-num_list.reverse()
-num = int(''.join(num_list))
-print('Число в двоичной системе = ', num)
+# user_n = int(input('Введите число: '))
+# n = user_n
+# num_list = []
+# while n > 0:
+#     num = n % 2
+#     num_list.append(str(num))
+#     n = n // 2
+# num_list.reverse()
+# num = int(''.join(num_list))
+# print('Число в двоичной системе = ', num)
+
+#_______________________________________________________________________
+
+# решение учителя
+
+def conv_bin(num:int):
+    list_nums = []
+    
+    while num > 0:
+        list_nums.insert(0, num % 2)
+        num //= 2
+        
+    print(*list_nums, sep = '')
+conv_bin(int(input()))        
+        
