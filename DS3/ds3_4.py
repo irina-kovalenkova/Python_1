97825761ca09c6a1a64644d1a9689e524ce25740
Iren <kovalenkovaiv@gmail.com>
2022-12-30 15:23:06 +0400

    fgg

--- DS3/ds3_4.py
+++ DS3/ds3_4.py
@@ -11,3 +11,36 @@
 # [9.26, 8.5, 1.14]
 # Min: 0.14, Max: 0.5. Difference: 0.36
 
+#_____________________________________________________
+
+#решение учителя
+
+from random import uniform
+
+def list_rand_nums(count: int):
+    list_nums = []
+    if count <= 0:
+        print('Negative value of the number of numbers!')
+        return [0.0]
+    
+    for i in range(count):
+        list_nums.append(round(uniform(1,count), 2))
+    return list_nums
+
+def dif_max_min(list_nums: list):
+    num_max = num_min = list_nums[0] % 1
+    
+    for k in range(1, len(list_nums)):
+        num = round(list_nums[k] % 1, 2)
+        if num > num_max:
+            num_max = num
+        elif num < num_min:
+            num_min = num
+            
+    rersult = round(num_max - num_min, 2)
+    print(f"Min: {num_min:.2f}, Max: {num_max:.2f}. Difference: {rersult}")
+    return rersult
+
+all_list = list_rand_nums(int(input("Number of numbers: ")))
+print(all_list)
+print(dif_max_min(all_list))    
--- DS3/ds3_5.py
+++ DS3/ds3_5.py
@@ -10,3 +10,18 @@
 # 5
 # out
 # 5 -3 2 -1 1 0 1 1 2 3 5
+
+#_________________________________________________________________
+
+def neg_fib(num: int):
+    a, b = 1, 1
+    list_nums = [0]
+    
+    for i in range(num):
+        list_nums.append(a)
+        list_nums.insert(0, a * (-1) ** i)
+        a, b = b, b + a
+        
+    return list_nums
+print(*neg_fib(int(input()))) 
+   
